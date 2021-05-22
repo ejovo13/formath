08f20cd24272ce2f1ed3adca3fb0cac92b01f30b
Evan Voyles <ejovo13@yahoo.com>
2021-05-22 04:16:12 +0200

    Test operator functions

--- src/test/vector/v_operator_test.f90
+++ src/test/vector/v_operator_test.f90
@@ -1,5 +1,5 @@
 program operator_test
-
+!! Test the functional operators
 use vector_m
 use iso_fortran_env
 
@@ -16,9 +16,49 @@ implicit none
 
     v3 = v1 .proj. v2
 
-    call v2%normalize()
-    call v3%normalize()
+    v2 = v2%normalized()
+    v3 = v3%normalized()
+    scal_res = v2 .dot. v3
+
+    print*, "Dot product of v2 and v3 = ", scal_res
 
     res = v1 + v2
+
+    v1 = [1, 5, 8]
+    v2 = [1, -3, -5]
+    res = v1 + v2
+    print*, "v1: ", v1%data(), " + ", v2%data(), " = ", res%data()
+
+    res = v1 - v2
+    print*, "v1: ", v1%data(), " - ", v2%data(), " = ", res%data()
+
+    res = v1 * 10
+    print*, "v1: ", v1%data(), " * 10 = ", res%data()
+
+    v2 = [100, 6, 5]
+    res = v2 / 10
+    print*, "v2: ", v2%data(), " /10 = ", res%data()
+
+    res = v1 * v2
+    print*, "v1: ", v1%data(), " * ", v2%data(), " = ", res%data()
+
+    res = v1 / v2
+    print*, "v1: ", v1%data(), " / ", v2%data(), " = ", res%data()
+
+    res = 5 * v1
+    print *, "5 * ", v1%data(), " = ", res%data()
+
+    res = 5 / v1
+    print *, "5 / ", v1%data(), " = ", res%data()
+
+    res = -v1
+    print *, "-(", v1%data(), ") = ", res%data()
+
+
+
+
+
+
+
 
 end program
