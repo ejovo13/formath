a8f016abb5cf56dd8c6c6ecb087213845ce7363e
Evan Voyles <ejovo13@yahoo.com>
2021-05-22 03:05:41 +0200

    Add copy constructor

--- src/test/vector/v_constructor_test.f90
+++ src/test/vector/v_constructor_test.f90
@@ -5,7 +5,7 @@ use vector_m
 
 implicit none
 
-    type(vector) :: v1, v2, v3, v4, v5, v6, v7
+    type(vector) :: v1, v2, v3, v4, v5, v6, v7, v8
 
     v1 = vector(5)                              !! Create empty vector(zeros) with 5 elements
     v2 = vector([1, 2, 3])                      !! Create dim 3 vector from [1, 2, 3]
@@ -14,6 +14,7 @@ implicit none
     v5 = vector(5, 1.6)    
     v6 = vector(10, 1.7d0)
     v7 = vector([1.4d0, -14.d0])
+    v8 = vector(v7)
 
     call v1%print_info()
     call v2%print_info()
@@ -22,6 +23,6 @@ implicit none
     call v5%print_info()
     call v6%print_info()
     call v7%print_info()
+    call v8%print_info()    
     
-
 end program
