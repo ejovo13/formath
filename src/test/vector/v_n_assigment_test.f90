ae2f5da6359558e90e34c983b8954cb5b8ebe33a
Evan Voyles <ejovo13@yahoo.com>
2021-05-22 03:05:57 +0200

    Test assigment speed compared to constructor speed

---
+++ src/test/vector/v_n_assigment_test.f90
@@ -0,0 +1,20 @@
+program v_n_assigment_test
+!! Test whether assigment or construction is faster
+use vector_m
+
+implicit none
+
+    type(vector) :: v1
+
+    integer :: i
+    integer, parameter :: N_LOOPS = 1E7
+
+    do i = 1, N_LOOPS
+
+        v1 = [1, 3, 4, 5]
+
+    end do
+
+    call v1%print_info()
+
+end program
