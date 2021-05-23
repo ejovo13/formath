6a716a05434c2d9c4c02bbf19e39596551e069bc
Evan Voyles <ejovo13@yahoo.com>
2021-05-23 19:28:15 +0200

    add more tests

---
+++ src/test/matrix/mat_subop_test.f90
@@ -0,0 +1,12 @@
+program mat_subop_test
+    ! Test the subroutine operators for matrices
+
+use matrix_m
+use vector_m
+use iso_fortran_env, only: real64
+
+implicit none
+
+    type(matrix) :: m1, m2, m3
+
+end program
